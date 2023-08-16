adfc3b6fb0a2437627b4215296f6aad970ec9814
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-16 21:06:32 +0000

    list-agenda

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,25 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: [],
+			user: null
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			obtenerInfo: async function() {
+				try {
+					let response = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda");
+					let data = await response.json();
+					console.log(data);
+
+					setStore({ contacts: data.results });
+					// setStore({propiedad:el valor que quieren actualizar})
+				} catch (error) {
+					console.log(error);
+				}
+			}
 		}
 	};
 };
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
 
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
 
 export const Contacts = () => {
+	const { store, actions } = useContext(Context);
+	const ( state,setState) = setState({
+		showModal:false
+	})
+console.log(store.contacts)
 	const [state, setState] = useState({
 		showModal: false
 	});
+
+	useEffect(function() {
+		actions.obtenerInfo();
+	}, []);
 
 	return (
 		<div className="container">
